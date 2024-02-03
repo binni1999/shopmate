ff19cf21a6b9269efff1c9f424752f333192182e
pankajbinwal001 <pankajbinwal38@gmail.com>
2024-02-03 20:08:38 +0530

    update context and other things

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import "./Header.css";
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { useCart } from "../context/CartContext";
 
 export const Header = () => {
+  const { cartList } = useCart();
   return (
     <header>
       <Link to={"/"} className="logo">
@@ -21,7 +23,7 @@ export const Header = () => {
         </NavLink>
       </nav>
       <Link to="/cart" className="items">
-        <span>Cart: 2</span>
+        <span>Cart: {cartList.length}</span>
       </Link>
     </header>
   );
--- src/components/ProductCart.js
+++ src/components/ProductCart.js
@@ -1,14 +1,30 @@
+import { useCart } from "../context/CartContext";
 import "./ProductCard.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const ProductCart = ({ product }) => {
+  const { addToCart, cartList, removeFromCart } = useCart();
+  const [isInCart, setIsInCart] = useState(false);
+  useEffect(() => {
+    const productIsInCart = cartList.find(
+      (cartItem) => cartItem.id === product.id
+    );
+    if (productIsInCart) setIsInCart(true);
+    else setIsInCart(false);
+  }, [cartList, product.id]);
   return (
     <div className="productcard">
       <img className="" src={product.image} alt={product.name} />
       <p className="name">{product.name}</p>
       <div className="action">
         <p>${product.price}</p>
-        <button>Add to cart</button>
+        {isInCart ? (
+          <button className="remove" onClick={() => removeFromCart(product)}>
+            Remove
+          </button>
+        ) : (
+          <button onClick={() => addToCart(product)}>Add to cart</button>
+        )}
       </div>
     </div>
   );
